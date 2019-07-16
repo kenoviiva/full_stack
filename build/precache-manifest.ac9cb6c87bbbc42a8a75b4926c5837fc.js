0922b0338ad595ce4880203f65e8af7fc0bfe500
Juha Savilampi <juha.savilampi@pp.inet.fi>
2019-07-16 14:54:32 +0300

    build included

---
+++ build/precache-manifest.ac9cb6c87bbbc42a8a75b4926c5837fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda8a09b8adcc9f03d9292274f28933e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd7557b9338c3c2d58d4",
+    "url": "/static/css/main.92909bef.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "cd7557b9338c3c2d58d4",
+    "url": "/static/js/main.853f2135.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
